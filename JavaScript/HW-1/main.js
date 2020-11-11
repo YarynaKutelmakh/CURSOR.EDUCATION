01f9d10fe1573b7dbf6bcfaacc8d08c7358fda2c
MaksymKutelmakh <yaryna.kutelmakh@gmail.com>
2020-11-11 08:31:52 +0200

    fixed mistakes from support

--- JavaScript/HW-1/main.js
+++ JavaScript/HW-1/main.js
@@ -1,37 +1,29 @@
-const chocolate = 15.678,
-      book = 123.965,
-      tea = 90.2345;
-
-let  sum = chocolate + book + tea,
-     sumFloat = Math.floor(chocolate) + Math.floor(book) + Math.floor(tea),
-     cashCustomer = 500,
-     sale = (Math.random() * 101).toFixed(0),
-     sumToPay = (sum - (sum / 100 * sale)).toFixed(2),
-     cost = sum / 2,
-     profit = cost - (sum - sumToPay);
+const chocolate = 15.678;
+const book = 123.965;
+const tea = 90.2345;
+const totalPrice = chocolate + book + tea;
+const sumFloat = Math.floor(chocolate) + Math.floor(book) + Math.floor(tea);
+const cashCustomer = 500;
+const sale = (Math.random() * 101).toFixed(0);
+const sumToPay = totalPrice - (totalPrice / 100 * sale);
+const cost = totalPrice / 2;
+const profit = cost - (totalPrice - sumToPay.toFixed(2));
+const isEven = sumFloat % 2 === 0;
 
 console.log(Math.max(chocolate, book, tea));
 console.log(Math.min(chocolate, book, tea));
-console.log(sum);
+console.log(totalPrice);
 console.log(sumFloat);
 console.log(Math.ceil(sumFloat / 100) * 100);
-
-let evenOrOdd;
-if (sumFloat % 2) {
-    evenOrOdd = false;
-} else {
-    evenOrOdd = true;
-}
-
-console.log(cashCustomer - sum);
-console.log((sum / 3).toFixed(2));
+console.log(cashCustomer - totalPrice);
+console.log((totalPrice / 3).toFixed(2));
 console.log(`Знижка: ${sale}%`);
-console.log(`Сума до оплати: ${sumToPay}`);
+console.log(`Сума до оплати: ${sumToPay.toFixed(2)}`);
 console.log(`Чистий прибуток: ${profit.toFixed(2)}`);
 
 document.writeln(`<i>Максимальне число: ${Math.max(chocolate, book, tea)} <br>
-                Мінімальне число: ${Math.min(chocolate, book, tea)} <br> Вартість всіх товарів: ${sum} <br> Заокруглена вартівсть: ${sumFloat} <br> 
-                Сума всіх товарів (округлена в меншу сторону) є парним числом: ${evenOrOdd} <br> Сума решти: ${cashCustomer - sum} <br> 
-                Cереднє значення цін : ${(sum / 3).toFixed(2)} <br>
-                <b>Знижка: ${sale}% <br> Сума до оплати: ${sumToPay} <br> Чистий прибуток: ${profit.toFixed(2)}
-                </b></i> `);
+                Мінімальне число: ${Math.min(chocolate, book, tea)} <br> Вартість всіх товарів: ${totalPrice} <br> 
+                Заокруглена вартівсть: ${sumFloat} <br> Сума всіх товарів (округлена в меншу сторону) є парним числом: ${isEven} <br> 
+                Сума решти: ${cashCustomer - totalPrice} <br> Cереднє значення цін : ${(totalPrice / 3).toFixed(2)} <br>
+                <b>Знижка: ${sale}% <br> Сума до оплати: ${sumToPay.toFixed(2)} <br> Чистий прибуток: ${profit.toFixed(2)}</b></i>`);
+
