621a6d52e7e10968a5f923f0d71b51dc60341444
MaksymKutelmakh <yaryna.kutelmakh@gmail.com>
2020-11-14 09:12:17 +0200

    add hw 2

---
+++ JavaScript/HW-2/js/main.js
@@ -0,0 +1,21 @@
+const warning = alert('Увага! Програма все конвертує в цілі числа.');
+
+let isOdd;
+document.getElementById('btn').onclick = () => {
+    const firstNumber = parseInt(document.getElementById('first-number').value);
+    const secondNumber = parseInt(document.getElementById('second-number').value);
+    let sum = 0;
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        if (isOdd) {
+            if (i % 2 === 0) {
+                continue;
+            }
+            sum += i;
+        } else {
+            sum += i;
+        }
+    }
+    document.getElementById('result').style.display = 'block';
+    document.getElementById('result').innerHTML = `Результат: ${sum}`
+}
+
