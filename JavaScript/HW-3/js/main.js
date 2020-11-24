2fb1c325bf07505ab0778d97f4cdd1ddff66ce86
MaksymKutelmakh <yaryna.kutelmakh@gmail.com>
2020-11-24 23:11:50 +0200

    fixed mistakes

--- JavaScript/HW-3/js/main.js
+++ JavaScript/HW-3/js/main.js
@@ -1,53 +1,59 @@
-let num = 1;
-function getPow (a, b) {
-    for(let i = 0; i < b; i++) {
-        num *= a;
-    }
-    return num;
+function getPow(a, b) {
+  let num = 1;
+  for (let i = 0; i < b; i++) {
+    num *= a;
+  }
+  return num;
 }
-console.log(getPow(5, 2));
+const variableForPowFunction = getPow(2, 3);
+console.log(variableForPowFunction);
 
 function countLetter(a, world) {
-    let counter = 0;
-    for(let i = 0; i < world.length; i++) {
-            const char = world[i].toLocaleLowerCase();
-            if (char === a.toLocaleLowerCase()) {
-                counter++;
-            }
-        }
-    console.log(counter);
+  let counter = 0;
+  for (let i = 0; i < world.length; i++) {
+    const char = world[i].toLocaleLowerCase();
+    if (char === a.toLocaleLowerCase()) {
+      counter++;
+    }
+  }
+  return counter; 
 }
-countLetter('o', 'woOOorld');
+const myCounter = countLetter('a', 'Abba');
+console.log(myCounter);
 
 function convertCurrency(currency) {
   if (currency.includes('$')) {
-      console.log(`${currency} = ${parseInt(currency) * 28} грн`)
+    return `${currency} = ${parseInt(currency) * 28} грн`
   } else if (currency.toLocaleLowerCase().includes('uah')) {
-      console.log(`${currency.toLocaleUpperCase()} = ${(parseInt(currency) / 28).toFixed(2)}$`)
+    return `${currency.toLocaleUpperCase()} = ${(parseInt(currency) / 28).toFixed(2)}$`
   } else {
-      console.log(`Error`);
+    return 'Error';
   }
 }
-convertCurrency('100uah');
+const exchanger = convertCurrency('100uah');
+console.log(exchanger);
 
-function maxNumber(currency) {
-  const numbersToString = currency.toString();
+function maxNumber(number) {
+  const numbersToString = number.toString();
   const arrayOfNumbers = numbersToString.split('').map(n => +n);
-  console.log(arrayOfNumbers.reduce(function(a, b) {
+  return arrayOfNumbers.reduce(function (a, b) {
     return Math.max(a, b);
-  }));
+  });
 }
-maxNumber(9351);
+const variableForMaxNumberFunction = maxNumber(9351);
+console.log(variableForMaxNumberFunction);
 
 function getSalary(salary) {
   const tax = 19.5;
   let result = +salary - (+salary / 100 * tax);
-  console.log(result);
+  return result;
 }
-getSalary(1000);
+const salary = getSalary(1000);
+console.log(salary);
 
 function formatName(name) {
   const correctName = name.toLocaleLowerCase();
-  console.log(correctName[0].toLocaleUpperCase() + correctName.slice(1));
+  return correctName[0].toLocaleUpperCase() + correctName.slice(1);
 }
-formatName('mAX');
+const name = formatName('mAX');
+console.log(name);
