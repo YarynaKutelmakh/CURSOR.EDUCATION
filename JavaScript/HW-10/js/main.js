00e34ba8f69cc0968cbf6d781519ddd8950b4e31
MaksymKutelmakh <yaryna.kutelmakh@gmail.com>
2020-12-17 22:30:14 +0200

    fixed ways

--- JavaScript/HW-10/js/main.js
+++ JavaScript/HW-10/js/main.js
@@ -2,11 +2,11 @@ const btns = document.querySelectorAll(".btns");
 const audio = new Audio();
 
 btns.forEach((btn)=>btn.addEventListener('click', function(e){
-    audio.src = `/sounds/${e.target.id}.mp3`;
+    audio.src = `sounds/${e.target.id}.mp3`;
     audio.play();
 }))
 
 document.addEventListener('keydown', function(e){
-    audio.src = `/sounds/${e.code}.mp3`;
+    audio.src = `sounds/${e.code}.mp3`;
     audio.play();
 })
